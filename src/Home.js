730b5bdc07d491575d52ac5433a9258e3273918b
Patrick <patrick_p12@hotmail.com>
2021-03-22 10:35:53 +0100

    Comments

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,8 @@ import "./App.css";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+/* hier wordt de functie aangemaakt met gebruik van useStates waarin data kan worden opgeslagen,
+ dus voor de afbeelding, naam, type en nummer. */
 function Home() {
   const [pokemon, setPokemon] = useState("Start your adventure!");
   const [pokemonImg, setPokemonImg] = useState();
@@ -9,6 +11,7 @@ function Home() {
   const [typepokemon, setTypepokemon] = useState();
   const [nummerpokemon, setNummerpokemon] = useState();
 
+  /* Hier maak ik een functie aan die de API aanroept en dan de data daarvan in de bovenstaande variabelen plaatst.*/
   function randomPokemon() {
     const randNum = Math.abs(Math.floor(Math.random() * (0 - 151)));
     fetch(`https://pokeapi.co/api/v2/pokemon/${randNum}`)
@@ -25,10 +28,11 @@ function Home() {
       })
       .catch(console.error);
   }
-
+/* Hier probeerde ik voor in mijn pokedex pagina een array aan te maken die de data plaatst van de Pokémon die al geweest zijn */
   const [pokemonArray, setPokemonArray] = useState([]);
   console.log(pokemonArray);
-
+/* In principe is dit eigelijk plain html wat ook gebruikt wordt in react om de content te laden in de pagina. 
+hier plaats ik de variabelen waar de data in zit.*/
   return (
     <div className="home">
       <Link to="/Pokedex">
@@ -36,12 +40,13 @@ function Home() {
           <img src="../pokedexicon.png" alt="pokedexicoon" />
         </div>
       </Link>
-
+      {/* Wat ik hier dus heb gedaan is in mijn html componenten de variabelen geplaatst waardoor de data daar wordt weergegeven
+      Daaronder met die classname heb ik een soort loopje gemaakt waardoor de classname checkt of hij overeenkomt met de type pokémon. Is dit waar?
+      geef dan de type pokemon de classname die hetzelfde is als de type van de pokemon waardoor er een kleurtje aan wordt gegeven. */}
       <h1>{wildpokemon}</h1>
       <img src={pokemonImg} />
       {nummerpokemon && <p className="pokemonnumber">#{nummerpokemon}</p>}
       <div className="naampokemon">{pokemon}</div>
-
       {typepokemon &&
         typepokemon.map((type) => (
           <div
@@ -83,7 +88,6 @@ function Home() {
             {type.type.name}
           </div>
         ))}
-
       <button className="catchknop" onClick={randomPokemon}>
         Catch Pokémon!
       </button>
